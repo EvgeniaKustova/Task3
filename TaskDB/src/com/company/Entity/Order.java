09db42f0f8ba18a29f976c75c42ee1fe1e680f85
evgenia <Kawaiibear@mail.ru>
2015-12-27 00:11:31 +0300

    Task3

--- TaskDB/src/com/company/Entity/Order.java
+++ TaskDB/src/com/company/Entity/Order.java
@@ -4,25 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    private int clientID;
     private int orderID;
-    private String productTitle;
     public List<Product> productList;
-
-    public int getClientID()
-    { return clientID; }
 
     public int getOrderID()
     { return orderID; }
 
-    public String getProductTitle()
-    { return productTitle; }
-
-    public void setClientID(int value){ clientID = value; }
-
     public void setOrderID(int value) { orderID = value; }
-
-    public void setProductTitle(String value) { productTitle = value; }
 
     public Order ()
     {
